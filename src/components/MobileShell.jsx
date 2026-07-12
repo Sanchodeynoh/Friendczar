8a841036ec79c64e7a0eb1a6e5e8a76ce66c8805
Denis Kamau <denomsancho5@gmail.com>
2026-07-12 16:53:25 +0300

    Add files via upload

--- src/components/MobileShell.jsx
+++ src/components/MobileShell.jsx
@@ -3,8 +3,8 @@ import BottomNav from "./BottomNav.jsx";
 
 export default function MobileShell({ children, header, showNav = true }) {
   return (
-    <div className="min-h-screen w-full flex justify-center bg-ink">
-      <div className="w-full max-w-[420px] flex flex-col h-screen">
+    <div className="w-full h-[100dvh] flex justify-center bg-ink overflow-hidden">
+      <div className="w-full max-w-[420px] flex flex-col h-full">
         {header}
         <main className="flex-1 min-h-0 relative flex flex-col">{children}</main>
         {showNav && <BottomNav />}
