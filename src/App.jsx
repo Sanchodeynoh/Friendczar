6517c176260f9289c071e4d7f0b4820c3e8877ff
Denis Kamau <denomsancho5@gmail.com>
2026-07-14 09:56:14 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Discover from "./pages/Discover.jsx";
+import Messages from "./pages/Messages.jsx";
+import Profile from "./pages/Profile.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import Admin from "./pages/Admin.jsx";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/discover" element={<Discover />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/messages/:threadId" element={<Messages />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/user/:userId" element={<UserProfile />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
+}
