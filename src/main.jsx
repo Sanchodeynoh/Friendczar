e79aefb0516e1e4f5985d41237bb0c0a44f07e6e
Denis Kamau <denomsancho5@gmail.com>
2026-07-16 23:37:04 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { SocketProvider } from "./context/SocketContext.jsx";
+import { CallProvider } from "./context/CallContext.jsx";
+import CallOverlay from "./components/CallOverlay.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HashRouter>
       <AuthProvider>
-        <App />
+        <SocketProvider>
+          <CallProvider>
+            <App />
+            <CallOverlay />
+          </CallProvider>
+        </SocketProvider>
       </AuthProvider>
     </HashRouter>
   </React.StrictMode>
