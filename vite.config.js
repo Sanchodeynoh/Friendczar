7bda87ce5f7a289cea4793650cbda7ab6d7e6265
Denis Kamau <denomsancho5@gmail.com>
2026-07-17 12:43:08 +0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: must match your repo name exactly (case-sensitive),
-  // e.g. https://sanchodeynoh.github.io/Friendczar/
-  base: "/Friendczar/",
-});
